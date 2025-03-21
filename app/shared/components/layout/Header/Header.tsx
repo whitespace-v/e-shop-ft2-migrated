46680c96c9aa66ff58db2742366d29105519f58c
ArchLinux <host@archlinux>
2025-03-21 17:25:02 +1000

    basket

--- app/shared/components/layout/Header/Header.tsx
+++ app/shared/components/layout/Header/Header.tsx
@@ -12,6 +12,11 @@ import { useDebounceCallback } from '@/app/shared/hooks/useDebounceCallback';
 import { useUserStore } from '@/app/shared/core/providers/userProvider';
 import { useRouter } from 'next/navigation';
 import { useLocalStorage } from '@/app/shared/hooks/useLocalStorage';
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
+import Link from 'next/link';
+import { useFavoriteStore } from '@/app/shared/core/providers/favoriteProvider';
+import { useBasketStore } from '@/app/shared/core/providers/basketProvider';
+import { Trash2 } from 'lucide-react';
 
 const Header = () => {
   const [value, setValue, removeValue] = useLocalStorage('token', '');
@@ -19,6 +24,8 @@ const Header = () => {
   const [isActiveInput, setIsActiveInput] = useState<boolean>(false);
   const { queryAction, query } = useSortStore(state => state);
   const { user_login, user_id, setUser } = useUserStore(state => state);
+  const { favorites, triggerFavorite } = useFavoriteStore(state => state);
+  const { basketItems, basketAction } = useBasketStore(state => state);
   const debounced = useDebounceCallback(queryAction, 500);
 
   useEffect(() => {
@@ -52,16 +59,67 @@ const Header = () => {
             </div>
           </div>
           <div className={s.buttons}>
-            <div className={s.buttons__item}>
-              <button className={s.buttons__btn}>
-                <WishList className={s.buttons__WishList} />
-              </button>
-            </div>
-            <div className={s.buttons__item}>
-              <button className={cn(s.buttons__btn, s.btn_cart)}>
-                <Cart className={s.buttons__Cart} />
-              </button>
-            </div>
+            <HoverCard>
+              <HoverCardTrigger>
+                <Link href={'/favorite'}>
+                  <div className={s.buttons__item}>
+                    <button className={s.buttons__btn}>
+                      <WishList className={s.buttons__WishList} />
+                    </button>
+                    {favorites.length > 0 && (
+                      <div className="rounded-full bg-red-500 p-1 flex items-center justify-center text-foreground-100 h-[24px] w-[24px]">
+                        {favorites.length}
+                      </div>
+                    )}
+                  </div>
+                </Link>
+              </HoverCardTrigger>
+              {favorites.length > 0 ? (
+                <HoverCardContent className="flex flex-col gap-2">
+                  {favorites.map((f, idx) => (
+                    <div key={idx} className="border border-gray-400 border-solid rounded-md flex justify-between">
+                      <Link href={'/products/' + f}>{f}</Link>
+                      <div onClick={() => void triggerFavorite(f)}>
+                        <Trash2 />
+                      </div>
+                    </div>
+                  ))}
+                </HoverCardContent>
+              ) : (
+                <HoverCardContent>В избранных товаров нет :(</HoverCardContent>
+              )}
+            </HoverCard>
+
+            <HoverCard>
+              <HoverCardTrigger>
+                <Link href={'/basket'}>
+                  <div className={s.buttons__item}>
+                    <button className={cn(s.buttons__btn, s.btn_cart)}>
+                      <Cart className={s.buttons__Cart} />
+                    </button>
+                    {basketItems.length > 0 && (
+                      <div className="rounded-full bg-red-500 p-1 flex items-center justify-center text-foreground-100 h-[24px] w-[24px]">
+                        {basketItems.length}
+                      </div>
+                    )}
+                  </div>
+                </Link>
+              </HoverCardTrigger>
+              {basketItems.length > 0 ? (
+                <HoverCardContent>
+                  {basketItems.map((i, idx) => (
+                    <div key={idx} className="border border-gray-400 border-solid rounded-md flex justify-between">
+                      <Link href={'/products/' + i}>{i}</Link>
+                      <div onClick={() => void basketAction(i)}>
+                        <Trash2 />
+                      </div>
+                    </div>
+                  ))}
+                </HoverCardContent>
+              ) : (
+                <HoverCardContent>В корзине товаров нет :(</HoverCardContent>
+              )}
+            </HoverCard>
 
             {user_id !== 0 ? (
               <div className="flex gap-4 items-center">
--- app/shared/components/layout/Product/Product.tsx
+++ app/shared/components/layout/Product/Product.tsx
@@ -6,32 +6,35 @@ import { IProduct } from '@/app/shared/types/IProduct';
 import Button from '../../UIKIT/Button/Button';
 import { useFavoriteStore } from '@/app/shared/core/providers/favoriteProvider';
 import { useBasketStore } from '@/app/shared/core/providers/basketProvider';
-import { useDislikeStore } from '@/app/shared/core/providers/dislikeProvider';
+import { toast } from 'sonner';
+import { Heart } from 'lucide-react';
 export const Product: React.FC<IProduct> = (props: IProduct) => {
   const { favorites, triggerFavorite } = useFavoriteStore(state => state);
 
   const { basketAction, basketItems } = useBasketStore(state => state);
 
-  const { dislikes, doLike, redoLike } = useDislikeStore(state => state);
   useEffect(() => {
     console.log(basketItems);
   }, [basketItems]);
 
+  const triggerBasket = () => {
+    if (basketItems.includes(props.id)) {
+      toast.info(`Продукт ${props.id} удален из корзины`);
+    } else {
+      toast.info(`Продукт ${props.id} добавлен в корзину`);
+    }
+    void basketAction(props.id);
+  };
   return (
     <div className={s.Product}>
       <div className={s.Product__like} onClick={() => void triggerFavorite(props.id)}>
-        {favorites.includes(props.id) ? '❤️' : '🤍'}
+        <Heart fill={favorites.includes(props.id) ? 'red' : 'none'} />
       </div>
-      <div onClick={() => doLike(props.id)}>Поставить 👎</div>
-      <div onClick={() => redoLike(props.id)}>Убрать 👎</div>
-      {dislikes.map(i => (
-        <> {i}</>
-      ))}
       <Image src={props.img} alt={props.title} width={160} height={160} className={s.Product__crad_image} />
       <h3 className={s.Product__title}>{props.title}</h3>
       <div className={s.Product__bottom}>
         <p className={s.Product__bottom_price}>${props.price}</p>
-        <Button size="m" style="black_solid" onClick={() => void basketAction(props.id)}>
+        <Button size="m" style="black_solid" onClick={() => triggerBasket()}>
           Buy now
         </Button>
       </div>
